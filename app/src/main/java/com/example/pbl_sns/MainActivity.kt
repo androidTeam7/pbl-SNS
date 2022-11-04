23c8e0d409eeeb7355ff66da3ca9043b62ab7753
babo0121 <ydbwlsjki60@gmail.com>
2022-11-04 21:54:03 +0900

    added bottomNavigation

--- app/src/main/java/com/example/pbl_sns/MainActivity.kt
+++ app/src/main/java/com/example/pbl_sns/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.pbl_sns
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private var bottomNavigationView: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        supportFragmentManager.beginTransaction().add(R.id.mainFrame, HomeFragment())
+            .commit()
+
+        bottomNavigationView?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.mainFrame, HomeFragment()).commit()
+                R.id.searchFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.mainFrame, SearchFragment()).commit()
+                R.id.profileFragment -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.mainFrame, ProfileFragment()).commit()
+            }
+            true
+        })
     }
 }
