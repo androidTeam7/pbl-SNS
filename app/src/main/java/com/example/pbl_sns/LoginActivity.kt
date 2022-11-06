fb9da5a6a89547feb334e97ac3f3f721ef38bb21
babo0121 <ydbwlsjki60@gmail.com>
2022-11-06 19:11:37 +0900

    add login function and SignupActivity

--- app/src/main/java/com/example/pbl_sns/LoginActivity.kt
+++ app/src/main/java/com/example/pbl_sns/LoginActivity.kt
@@ -1,11 +1,45 @@
 package com.example.pbl_sns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.pbl_sns.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonLogin.setOnClickListener {
+            val userEmail = binding.editTextId.text.toString()
+            val password = binding.editTextPasswd.text.toString()
+            doLogin(userEmail, password)
+        }
+
+        binding.goSignUp.setOnClickListener {
+            startAcitivity(
+
+            )
+        }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginAcitivy", "signInWIthEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
