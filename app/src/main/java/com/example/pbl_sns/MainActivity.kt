c98c6007d05448871e076404df8372e85834cdd4
babo0121 <ydbwlsjki60@gmail.com>
2022-11-06 21:03:03 +0900

    MainActivity 주석 추가

--- app/src/main/java/com/example/pbl_sns/MainActivity.kt
+++ app/src/main/java/com/example/pbl_sns/MainActivity.kt
@@ -18,25 +18,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // FirebaseUser 객체가 null인 경우
         if(Firebase.auth.currentUser == null) {
-            startActivity(
+            startActivity(   // LoginActivity로 이동
                 Intent(this, LoginActivity::class.java))
-            finish()
+            finish()    // 현재 화면 지우기
         }
 
+        // bottomNavigationView 가져오기
         bottomNavigationView = binding.bottomNavigationView
 
+        // HomeFragment로 시작
         supportFragmentManager.beginTransaction().add(R.id.mainFrame, HomeFragment())
             .commit()
 
+        // bottomNavigation 선택에 따라 처리
         bottomNavigationView?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.homeFragment -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.mainFrame, HomeFragment()).commit()
+                    .replace(R.id.mainFrame, HomeFragment()).commit()   // 화면을 HomeFragment로
                 R.id.searchFragment -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.mainFrame, SearchFragment()).commit()
+                    .replace(R.id.mainFrame, SearchFragment()).commit()  // 화면을 SearchFragment로
                 R.id.profileFragment -> supportFragmentManager.beginTransaction()
-                    .replace(R.id.mainFrame, ProfileFragment()).commit()
+                    .replace(R.id.mainFrame, ProfileFragment()).commit()   // 화면을 ProfileFragment로
             }
             true
         })
